446899dd7b12b0b8fc676075d39d4402c8b7cf9d
Tiffany Wong <wongt1227@gmail.com>
2021-09-11 15:14:46 -0400

    number guessing game

---
+++ week_01_python/GuessMyNumber.py
@@ -0,0 +1,23 @@
+import random
+
+#secret number chosen randomly
+number = random.randint(1,100)
+
+#prompt user to guess number
+print('\nI am thinking of a number between 1 and 100, inclusive.')
+print('Can you guess what it is?')
+print('\nType in a number: ')
+guess = input()
+
+
+if guess > number:
+    difference = guess - number
+elif number > guess:
+    difference = number - guess
+elif number == guess:
+    difference = 0
+    print('\nCongrats! You guessed the secret number.')
+
+print('\nYour guess was: ' + str(guess))
+print('The number I was thinking of was: ' + str(number))
+print('You were off by: ' + str(difference))
